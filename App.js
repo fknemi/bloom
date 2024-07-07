6d831bd2358fa314c6b1cb65e1d1c3b4f110439b
fknemi <vihaan.s310@gmail.com>
2024-07-08 04:16:55 +0530

    fixed focused tabs to correctly update color of text and icon

--- App.js
+++ App.js
@@ -8,39 +8,6 @@ import FilterIcon from "./app/components/icons/FilterIcon";
 import SearchIcon from "./app/components/icons/SearchIcon";
 import { Text, View, StatusBar } from "react-native";
 import useFonts from "./app/utils/useFonts"
-import { useEffect } from "react";
-// {{
-//   headerShown: false,
-//   tabBarStyle: {
-//     backgroundColor: "#D5C9B9",
-
-//   },
-//         tabBarLabelStyle: {
-//             color: "#000",
-//             textTransform: "capitalize",
-//                 textAlign: "center",
-
-//         }
-
-// }
-
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -48,78 +15,61 @@ const Tab = createBottomTabNavigator()
 
 export default function App() {
 
-        const fonts = useFonts();
+    const fonts = useFonts();
 
-        if (!fonts) {
-            return <View><Text>Loading</Text></View>
+    if (!fonts) {
+        return <View><Text>Loading</Text></View>
 
-        }
+    }
 
 
     return (
         <NavigationContainer>
- <StatusBar
-        animated={true}
-        backgroundColor="transparent"
-        barStyle={'dark-content'}
-        translucent={true}
-        
-      />
+            <StatusBar
+                animated={true}
+                backgroundColor="transparent"
+                barStyle={'dark-content'}
+                translucent={true}
+
+            />
             <Tab.Navigator
                 screenOptions={({ route }) => ({
                     tabBarLabel: ({ focused }) => {
                         return <Text className={`${focused ? "color-black" : "color-peach-dark"} font-pixer capitalize mb-2 text-lg`} >{route.name}</Text>
                     },
+                    tabBarIcon: ({ focused }) => {
+                        if (route.name === SCREENS.DASHBOARD_SCREEN) {
+                            return <HomeIcon focused={focused} />
+                        } else if (route.name === SCREENS.EDITOR_SCREEN) {
+                            return <PenIcon focused={focused} />
+                        } else if (route.name === SCREENS.SEARCH_SCREEN) {
+                            return <SearchIcon focused={focused} />
+                        } else if (route.name === SCREENS.FILTER_SCREEN) {
+                            return <FilterIcon focused={focused} />
+                        }
+                    },
+
                     tabBarStyle: {
                         height: 80,
                         backgroundColor: "#D5C9B9",
 
                     },
                     headerShown: false
+                })
                 }
-                )}
-
-            // tabBarIcon: ({ focused }) => {
-            // let iconSource;
-
-            // if (route.name === 'Map') {
-            //   iconSource = require('./assets/public.png');
-            // } else if (route.name === 'List') {
-            //   iconSource = require('./assets/numbered.png');
-            // } else if (route.name === 'Log') {
-            //   iconSource = require('./assets/menu.png');
-            // } else if (route.name === 'Talk') {
-            //   iconSource = require('./assets/chat.png');
-            // } else if (route.name === 'Account') {
-            //   iconSource = require('./assets/account.png');
-            // } 
-
-            // const icon = <Image
-            //   style={{
-            //     width: 25,
-            //     height: 25,
-            //     tintColor: focused ? colors.primary : colors.inactive,
-            //     marginTop: focused ? 5 : 15
-            //   }}
-            //   source={iconSource}
-            // /> ;
-
-            // return icon
-            // },
-
             >
 
                 <Tab.Screen name={SCREENS.DASHBOARD_SCREEN} component={DashboardScreenNavigator} options={{
-                    tabBarIcon: ({ focused }) => <HomeIcon route={focused} />
+
                 }} />
                 <Tab.Screen name={SCREENS.EDITOR_SCREEN} component={EditorScreenNavigator} options={{
-                    tabBarIcon: ({ focused }) => <PenIcon focused={focused} />
+
                 }} />
                 <Tab.Screen name={SCREENS.SEARCH_SCREEN} component={SearchScreenNavigator} options={{
-                    tabBarIcon: ({ focused }) => <SearchIcon focused={focused} />
+
                 }} />
                 <Tab.Screen name={SCREENS.FILTER_SCREEN} component={FilterScreenNavigator} options={{
-                    tabBarIcon: ({ focused }) => <FilterIcon focused={focused} />
+
                 }} />
             </Tab.Navigator>
 
--- app/components/icons/FilterIcon.js
+++ app/components/icons/FilterIcon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Svg, { Path } from 'react-native-svg';
 
-const FilterIcon = ({width, height}) => {
+const FilterIcon = ({width, height, focused}) => {
 
     return (
         <Svg
@@ -13,8 +13,8 @@ const FilterIcon = ({width, height}) => {
       >
         <Path
           d="M4.33334 5.66668H17.6667V8.33334H4.33334V5.66668ZM0.333344 0.333344H21.6667V3.00001H0.333344V0.333344ZM8.33334 11H13.6667V13.6667H8.33334V11Z"
-          fill="#9C9388"
-        />
+          fill={focused ? "#000" : "#9C9388"}
+          />
       </Svg>
       
       
